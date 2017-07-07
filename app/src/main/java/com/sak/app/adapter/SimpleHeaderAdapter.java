986114d792a31dd62c2ac77d0bf49b3ee87d0afe
IVRING <1141502381@qq.com>
2017-07-07 14:23:39 +0800

    Initial commit

---
+++ app/src/main/java/com/sak/app/adapter/SimpleHeaderAdapter.java
@@ -0,0 +1,55 @@
+package com.sak.app.adapter;
+
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.sak.app.R;
+import com.sak.ultilviewlib.adapter.BaseHeaderAdapter;
+
+/**
+ * Created by engineer on 2017/4/30.
+ */
+
+public class SimpleHeaderAdapter extends BaseHeaderAdapter {
+    private ImageView loading;
+
+    public SimpleHeaderAdapter(Context context) {
+        super(context);
+    }
+
+    @Override
+    public View getHeaderView() {
+        View mView = mInflater.inflate(R.layout.simple_header_refresh_layout, null, false);
+        loading = (ImageView) mView.findViewById(R.id.loading);
+        return mView;
+    }
+
+    @Override
+    public void pullViewToRefreshDeltaY(int deltaY) {
+        loading.setImageResource(R.drawable.simple_loading);
+        AnimationDrawable mAnimationDrawable= (AnimationDrawable) loading.getDrawable();
+        mAnimationDrawable.start();
+    }
+
+    @Override
+    public void pullViewToRefreshScale(float scale) {
+
+    }
+
+    @Override
+    public void releaseViewToRefresh(int deltaY) {
+
+    }
+
+    @Override
+    public void headerRefreshing() {
+
+    }
+
+    @Override
+    public void headerRefreshComplete() {
+    }
+
+}
---
+++ app/src/main/java/com/sak/app/subfragment/ListViewFragment.java
@@ -0,0 +1,91 @@
+package com.sak.app.subfragment;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.sak.app.R;
+import com.sak.app.adapter.MeiTuanHeaderAdapter;
+import com.sak.ultilviewlib.UltimateRefreshView;
+import com.sak.ultilviewlib.interfaces.OnFooterRefreshListener;
+import com.sak.ultilviewlib.interfaces.OnHeaderRefreshListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Activities that contain this fragment must implement the
+ * create an instance of this fragment.
+ */
+public class ListViewFragment extends Fragment {
+    private UltimateRefreshView mUltimateRefreshView;
+
+    private int page = 0;
+    private int PER_PAGE_NUM = 15;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_list_view, container, false);
+        initView(view);
+        return view;
+    }
+
+    private void initView(View view) {
+        View headview = LayoutInflater.from(getContext()).inflate(R.layout.list_headview_layout,
+                null, false);
+        ListView listView = (ListView) view.findViewById(R.id.listView);
+        final List<String> datas = new ArrayList<>();
+        for (int i = 0; i < PER_PAGE_NUM; i++) {
+            datas.add("this is item " + i);
+        }
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, datas);
+        listView.setAdapter(adapter);
+        listView.addHeaderView(headview);
+        mUltimateRefreshView = (UltimateRefreshView) view.findViewById(R.id.refreshView);
+        mUltimateRefreshView.setBaseHeaderAdapter(new MeiTuanHeaderAdapter(getContext()));
+        mUltimateRefreshView.setBaseFooterAdapter();
+        mUltimateRefreshView.setOnHeaderRefreshListener(new OnHeaderRefreshListener() {
+            @Override
+            public void onHeaderRefresh(UltimateRefreshView view) {
+                page = 0;
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        datas.clear();
+                        for (int i = page * PER_PAGE_NUM; i < PER_PAGE_NUM; i++) {
+                            datas.add("this is item " + i);
+                        }
+                        adapter.notifyDataSetChanged();
+                        mUltimateRefreshView.onHeaderRefreshComplete();
+                    }
+                }, 2000);
+            }
+        });
+
+        mUltimateRefreshView.setOnFooterRefreshListener(new OnFooterRefreshListener() {
+            @Override
+            public void onFooterRefresh(UltimateRefreshView view) {
+                page++;
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (int i = page * PER_PAGE_NUM; i < (page + 1) * PER_PAGE_NUM; i++) {
+                            datas.add("this is item " + i);
+                        }
+                        adapter.notifyDataSetChanged();
+                        mUltimateRefreshView.onFooterRefreshComplete();
+                    }
+                }, 200);
+            }
+        });
+    }
+
+}
